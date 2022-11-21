81d9a682b6333f90dbd43a4ba899f8d9f66ea476
AugmentedDuck <anders.m.matzen@gmail.com>
2022-11-21 21:04:28 +0100

    Made it able to compare every element
    It's not the most efficient comparator, some of them you could guess from previous comparisons. But it is simple

--- sketch.js
+++ sketch.js
@@ -1,7 +1,58 @@
+const unsortedArray = [];
+const sortedArray = [];
+const nrOfElements = 35
+const currentComp = [0, 0]
+let leftButton;
+let rightButton;
+
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(600, 400);
+  background(200)
+
+  leftButton = createButton("This one")
+  leftButton.size(50, 50)
+  leftButton.position(10, height / 1.5);
+  leftButton.mousePressed(chooseLeft)
+
+  rightButton = createButton("This one")
+  rightButton.size(50, 50)
+  rightButton.position(width - 50 - 10, height / 1.5);
+  rightButton.mousePressed(chooseRight)
+  
+  for (let i = 0; i < nrOfElements; i++){
+    unsortedArray[i] = int(random(nrOfElements))
+  }
+  nextComp(currentComp[0], currentComp[1])
 }
 
 function draw() {
-  background(220);
+
+}
+
+function chooseLeft(){
+
+
+  nextComp(currentComp[0], currentComp[1])
+}
+
+function chooseRight(){
+
+
+  nextComp(currentComp[0], currentComp[1])
+}
+
+function nextComp(i, j){
+  if (i < unsortedArray.length){
+    if (j < nrOfElements){
+      j++
+    } else {
+      j = i + 1
+      i++
+      j++
+    }
+  }
+  
+  currentComp[0] = i
+  currentComp[1] = j
+  console.log(currentComp)
 }
